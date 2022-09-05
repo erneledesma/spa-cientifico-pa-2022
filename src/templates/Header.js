f7a95583a566072e33f63913d1912785a8a44635
Ernesto Ledesma <eeledesma@teco.com.ar>
2022-09-05 20:21:29 -0300

    [ADD][feature]: home pages & routes

---
+++ src/templates/Header.js
@@ -0,0 +1,20 @@
+
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a href="/">Cientificos App</a>
+                </h1>
+            </div>
+
+            <div class="Header-nav">
+                <a href="#/about/">
+                </a>
+            </div>
+        </div>   
+    `
+   return view; 
+}
+
+export default Header;
